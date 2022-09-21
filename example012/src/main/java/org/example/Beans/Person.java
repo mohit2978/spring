9a15ae0771037b6a5111d04cbb8a01a28fe76127
mohit2978 <mohitrakheja007@gmail.com>
2022-09-21 14:40:47 +0530

    SetterInjection

---
+++ example012/src/main/java/org/example/Beans/Person.java
@@ -0,0 +1,33 @@
+package org.example.Beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Person {
+    public Person(){
+        System.out.println("person created by spring");
+    }
+    private String name;
+    private Vehicle v;
+
+    @PostConstruct
+    public void setValues() {
+        this.name = "Mohit";
+    }
+
+    @Autowired
+    public void setV(Vehicle v) {
+        this.v = v;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", v=" + v +
+                '}';
+    }
+}
